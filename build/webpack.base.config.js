8b4c7cbe1ef1573f6415f7f6d41187d97f76718e
kid <dingyi@cmiot.chinamobile.com>
2019-11-05 09:51:50 +0800

    update template

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -7,17 +7,22 @@ const Webpack = require('webpack');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: ['@babel/polyfill', './src/index.js'],
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
+        loader: 'vue-loader',
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
-        include: path.resolve(__dirname, '../src')
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        }
       },
       {
         test: /\.(png|jpg|gif)$/,
@@ -25,8 +30,8 @@ module.exports = {
           {
             loader: 'url-loader',
             options: {
-              limit: 8192
-            }
+              limit: 8192,
+            },
           }
         ]
       }
@@ -44,13 +49,13 @@ module.exports = {
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../index.html'),
-      inject: true
+      inject: true,
     }),
     new Webpack.NamedModulesPlugin()
   ],
   optimization: {
     splitChunks: {
-      chunks: 'async'
+      chunks: 'async',
     }
   }
 };
